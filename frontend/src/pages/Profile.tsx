7c165cdffb23c2544e3496959cf5ab358547afbb
Aniket Mahajan <mahajananiket207@gmail.com>
2025-04-23 09:22:10 +0530

    create profile dashboard

--- frontend/src/pages/Profile.tsx
+++ frontend/src/pages/Profile.tsx
@@ -1,4 +1,4 @@
-import { Eye, PenSquare, Trash2, User } from "lucide-react"
+import { Eye, LogOut, PenSquare, Trash2, User } from "lucide-react"
 
 export const Profile = () => {
 
@@ -10,20 +10,45 @@ export const Profile = () => {
         <div className="flex flex-col items-center w-[80%] h-[80%] bg-yellow-300 rounded-2xl shadow-xl">
         
         {/* 1 user img and name div */}
-          <div className="flex items-center bg-blue-400 p-2 w-[90%] mt-5">
-            <div className="h-20 w-20 bg-white rounded-full flex items-center justify-center ml-8">
-              <img src="C:\Users\gaura\Videos\Screenshots" />
+          <div className="flex items-center bg-blue-400 p-2 w-[90%] mt-5 rounded-lg">
+            <div className="h-20 w-20 bg-white rounded-full flex items-center justify-center ml-4">
+              <img 
+                src="C:\Users\gaura\Videos\Screenshots" 
+                alt="Profile"
+                className="h-full w-full rounded-full object-cover"
+                />
             </div>
-            <div className="ml-8 h-20 p-4">
+            <div className="ml-6 flex flex-col justify-center">
               <h3 className="font-bold text-xl text-custom-ash-900">
                 John Doe
               </h3>
               <span className="text-sm text-gray-800">john@doegmail.com</span>
             </div>
+            <div className="ml-12 text-center">
+              <h4 className="font-semibold">
+                Posts
+              </h4>
+              <p className="text-lg font-bold">30</p>
+            </div>
+
+            <div className="ml-auto mr-6 flex space-x-4">
+              <button className="bg-white text-blue-600 px-4 py-2 rounded-md font-semibold shadow hover:bg-blue-100 transition">
+                Detailes
+              </button>
+              <button className="bg-white text-blue-600 px-4 py-2 rounded-md font-semibold shadow hover:bg-blue-100 transition">
+                Update Profile
+              </button>
+              <button className="bg-white text-blue-600 px-4 py-2 rounded-md font-semibold shadow hover:bg-blue-100 transition">
+                Saved Posts
+              </button>
+            </div>
+
           </div>
         {/* 1 div end */}
 
         {/* 2 user post stats */}
+
+        {/* if there is no blog created then show a message */}
           <div className="mt-4">
            
           <div className="overflow-x-auto">
@@ -111,8 +136,14 @@ export const Profile = () => {
             </table>
           </div> 
 
-          </div>
+        </div>
         {/* 2 div end */}
+
+        <div className="mt-auto mr-auto  ml-12 mb-3 bottom-4 left-4">
+          <button className="py-2 px-4 border text-custom-ash-900  rounded-lg">
+            <LogOut size={20} className="hover:text-red-400"/>
+          </button>
+        </div>
         </div>
       </div>
     </div>
